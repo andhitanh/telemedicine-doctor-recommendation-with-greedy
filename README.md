fafae960a16d3df526e1c6c4f39649166c939aee
andhitanh <onecodhita@gmail.com>
2024-06-13 02:05:56 +0700

    feat : add readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # MakalahStima
-Implementasi Algoritma Greedy dalam Pemilihan Dokter untuk Konsultasi Daring pada Aplikasi Halodoc 
+Implementasi Algoritma Greedy dalam Pemilihan Dokter untuk Konsultasi Daring pada Aplikasi Telemedisin
+
+## Table of Contents
+* [General Information](#general-information)
+* [Creator](#creator)
+* [Features](#features)
+* [Requirements](#requirements)
+* [How to Run](#how-to-run)
+
+## General Information
+Program ini menggunakan Algoritma Greedy untuk menyelesaikan persoalan perumusan daftar rekomendasi dokter untuk konsultasi daring pada aplikasi telemedisin. Program akan menerima masukan berupa spesialisasi dari dokter yang pasien inginkan, jangka waktu konsultasi yang pasien inginkan, dan jumlah dokter yang pasien inginkan ada dalam daftar rekomendasi yang dihasilkan. Kemudian, program akan mengembalikan daftar rekomendasi sesuai kriteria pasien jika terdapat data dokter-dokter yang memenuhi.
+
+## Creator
+| NIM      | Nama                    | Kelas                                                                                                                                                                                                               |
+|----------|-------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 13522060 | Andhita Naura Hariyanto    | K-02                                                              |
+
+## Features
+Menampilkan daftar rekomendasi dokter dari data dokter yang ada pada program simulasi pembuatan daftar rekomendasi dokter pada aplikasi telemedis berdasarkan 4 pilihan strategi greedy, yaitu Greedy by Fee, Greedy by Rating, Greedy by Experience, dan Greedy by Overall Aspect
+
+## Requirements
+Anda harus menginstall bahasa python dan library pandas untuk menjalankan program ini
+
+## How to Run
+1. Buka terminal and clone repository berikut (https://github.com/andhitanh/GreedyAlgorithmForRecommendationList.git)
+
+2. Buka folder src dengan menjalankan command berikut pada folder utama di
+```
+cd src
+```
+
+3. Jalankan program utama dengan perintah berikut
+
+```
+python main.py
+```
--- src/main.py
+++ src/main.py
@@ -2,55 +2,73 @@ import csv
 from data_dokter import *
 from fungsi_seleksi import *
 
-filename = 'data/tes.csv'
+def main():
+    filename = '../data/tes.csv'
 
-print("Selamat datang di Simulasi Pembuatan Daftar Rekomendasi Dokter untuk Aplikasi Telemedisin!")
+    print("Selamat datang di Simulasi Pembuatan Daftar Rekomendasi Dokter untuk Aplikasi Telemedisin!")
 
-nama = input("Agar bisa melakukan pelayanan dengan lebih baik, masukkan nama Anda: ")
+    nama = input("Agar bisa melakukan pelayanan dengan lebih baik, masukkan nama Anda: ")
 
-print()
-print("Selamat datang,", nama, "!")
-print("Kami akan membantu Anda menemukan dokter yang sesuai dengan kebutuhan Anda.\n")
-print("Berikut adalah spesialisasi yang tersedia: ")
-print("1. Dokter Umum\n2. Anak\n3. Penyakit Dalam\n4. Mata\n")
-specialization = int(input("Masukkan nomor spesialisasi yang Anda butuhkan (1/2/3/4): "))
-
-if (specialization == 1) :
-    specialization_choice = "Dokter Umum"
-elif (specialization == 2) :
-    specialization_choice = "Spesialis Anak"
-elif (specialization == 3) :
-    specialization_choice = "Spesialis Penyakit Dalam"
-else :
-    specialization_choice = "Spesialis Mata"
-
-print()
-print("Anda memilih pelayanan dengan", specialization_choice, "\n")
-
-day = input(("Masukkan hari konsultasi yang Anda inginkan: "))
-
-print("Anda hanya dapat memilih waktu konsultasi antara pukul 08:00 hingga 21:00")
-print("Masukkan jangka waktu konsultasi yang Anda inginkan: ")
-hour_start, minute_start = map(int, input("Masukkan jangka awal konsultasi dengan format JJ:MM (misal 08:00): ").split(':'))
-hour_end, minute_end = map(int, input("Masukkan jangka akhir konsultasi dengan format JJ:MM (misal 21:00): ").split(':'))
-
-print()
-doctor_number = int(input("Masukkan jumlah maksimal dokter yang Anda inginkan dalam daftar rekomendasi: "))
-
-print()
-print("Berikut adalah pilihan metode pengurutan dokter yang dapat Anda pilih:")
-print("1. Pengurutan berdasarkan harga\n2. Pengurutan berdasarkan rating\n3. Pengurutan berdasarkan pengalaman\n4. Generator rekomendasi dokter secara menyeluruh\n")
-greedy_method = int(input("Masukkan nomor metode pengurutan yang Anda inginkan (1/2/3/4): "))
-
-if greedy_method == 4 :
-    doctor_list = density_selection_function(filename)
-    result = greedy_by_density(doctor_list, specialization_choice, hour_start, minute_start, hour_end, minute_end, day, doctor_number)
-else :
-    result = greedy_by_not_density(filename, greedy_method, specialization_choice, hour_start, minute_start, hour_end, minute_end, day, doctor_number)
-
-if (result) :
     print()
-    print("Berikut adalah daftar rekomendasi dokter yang sesuai dengan kriteria Anda: ")
-    for i in range(len(result)):
-        print(f"{i+1}. {result[i].name} (Jadwal: {result[i].start_hour:02}:{result[i].start_minute:02}-{result[i].end_hour:02}:{result[i].end_minute:02})")
-        print(f"Rating : {result[i].rating}, Pengalaman : {result[i].years_experience} tahun, Biaya Konsultasi : Rp {result[i].fee}")
+    print("Selamat datang,", nama, "!")
+    print("Kami akan membantu Anda menemukan dokter yang sesuai dengan kebutuhan Anda.\n")
+    print("Berikut adalah spesialisasi yang tersedia: ")
+    print("1. Dokter Umum\n2. Anak\n3. Penyakit Dalam\n4. Mata\n")
+    specialization = int(input("Masukkan nomor spesialisasi yang Anda butuhkan (1/2/3/4): "))
+
+    if (specialization == 1) :
+        specialization_choice = "Dokter Umum"
+    elif (specialization == 2) :
+        specialization_choice = "Spesialis Anak"
+    elif (specialization == 3) :
+        specialization_choice = "Spesialis Penyakit Dalam"
+    else :
+        specialization_choice = "Spesialis Mata"
+
+    print()
+    print("Anda memilih pelayanan dengan", specialization_choice, "\n")
+
+    day = input(("Masukkan hari konsultasi yang Anda inginkan: "))
+
+    print("Anda hanya dapat memilih waktu konsultasi antara pukul 08:00 hingga 21:00")
+    print("Masukkan jangka waktu konsultasi yang Anda inginkan: ")
+    hour_start, minute_start = map(int, input("Masukkan jangka awal konsultasi dengan format JJ:MM (misal 08:00): ").split(':'))
+    hour_end, minute_end = map(int, input("Masukkan jangka akhir konsultasi dengan format JJ:MM (misal 21:00): ").split(':'))
+
+    print()
+    doctor_number = int(input("Masukkan jumlah maksimal dokter yang Anda inginkan dalam daftar rekomendasi: "))
+
+    print()
+    print("Berikut adalah pilihan metode pengurutan dokter yang dapat Anda pilih:")
+    print("1. Pengurutan berdasarkan harga\n2. Pengurutan berdasarkan rating\n3. Pengurutan berdasarkan pengalaman\n4. Generator rekomendasi dokter secara menyeluruh\n")
+    greedy_method = int(input("Masukkan nomor metode pengurutan yang Anda inginkan (1/2/3/4): "))
+
+    if greedy_method == 4 :
+        doctor_list = density_selection_function(filename)
+        result = greedy_by_density(doctor_list, specialization_choice, hour_start, minute_start, hour_end, minute_end, day, doctor_number)
+    else :
+        result = greedy_by_not_density(filename, greedy_method, specialization_choice, hour_start, minute_start, hour_end, minute_end, day, doctor_number)
+
+    if (result) :
+        print()
+        print("Berikut adalah daftar rekomendasi dokter yang sesuai dengan kriteria Anda: ")
+        for i in range(len(result)):
+            print(f"{i+1}. {result[i].name} (Jadwal: {result[i].start_hour:02}:{result[i].start_minute:02}-{result[i].end_hour:02}:{result[i].end_minute:02})")
+            print(f"Rating : {result[i].rating}, Pengalaman : {result[i].years_experience} tahun, Biaya Konsultasi : Rp {result[i].fee}")
+
+    print()
+    finalchoice = input("Ingin kembali menggunakan aplikasi? (y/n)")
+
+    while (finalchoice.lower() != "y" and finalchoice.lower() != "n") :
+        print("Input salah!")
+        print()
+        finalchoice = input("Ingin kembali menggunakan aplikasi? (y/n) ")
+
+    if finalchoice.lower() == "y" :
+        main()
+    elif finalchoice.lower() == "n" :
+        print("Sampai jumpa kembali! Semoga sehat selalu!")
+        exit()
+
+if __name__ == "__main__":
+    main()
